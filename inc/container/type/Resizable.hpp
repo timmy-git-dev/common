0e054e899330eb51b2cedc391f5517bda087bbe5
timmy-git-dev <timmy.mail.dev@proton.me>
2026-06-10 14:18:02 -0600

    Simple string class using inheritance, may swap to composition?

--- inc/container/type/Resizable.hpp
+++ inc/container/type/Resizable.hpp
@@ -7,12 +7,12 @@ namespace cmn::container::type
     struct Resizable
     {
 public:
-        void append_copy(const TYPE_  &_element                  ) {static_cast<CONTAINER_*>(this)->append_copy__(_element        );}
-        void append_move(      TYPE_ &&_element                  ) {static_cast<CONTAINER_*>(this)->append_move__(_element        );}
-        void insert_copy(const TYPE_  &_element, const s64 _index) {static_cast<CONTAINER_*>(this)->insert_copy__(_element, _index);}
-        void insert_move(      TYPE_ &&_element, const s64 _index) {static_cast<CONTAINER_*>(this)->insert_move__(_element, _index);}
-        void remove     (                                        ) {static_cast<CONTAINER_*>(this)->remove__     (                );}
-        void remove     (                        const s64 _index) {static_cast<CONTAINER_*>(this)->remove__     (          _index);}
+        void append_copy(TYPE_   _element                  ) {static_cast<CONTAINER_*>(this)->append_copy__(_element        );}
+        void append_move(TYPE_ &&_element                  ) {static_cast<CONTAINER_*>(this)->append_move__(_element        );}
+        void insert_copy(TYPE_   _element, const s64 _index) {static_cast<CONTAINER_*>(this)->insert_copy__(_element, _index);}
+        void insert_move(TYPE_ &&_element, const s64 _index) {static_cast<CONTAINER_*>(this)->insert_move__(_element, _index);}
+        void remove     (                                  ) {static_cast<CONTAINER_*>(this)->remove__     (                );}
+        void remove     (                  const s64 _index) {static_cast<CONTAINER_*>(this)->remove__     (          _index);}
 
         void reserve(const s64 _capacity) {static_cast<CONTAINER_*>(this)->reserve__(_capacity);}
     };
