6e98435b9989e0e9e982610fe2ee38c976c11751
timmy-git-dev <timmy.mail.dev@proton.me>
2026-07-12 13:01:22 -0600

    - maybe proper mac syscalls?

--- inc/system/syscall/Func.hpp
+++ inc/system/syscall/Func.hpp
@@ -6,9 +6,9 @@
 namespace cmn::system::syscall
 {
     #if CMN_SYSTEM_ARCH_ARM64
-    inline long syscall(long _type, long _arg0 = 0, long _arg1 = 0, long _arg2 = 0, long _arg3 = 0, long _arg4 = 0, long _arg5 = 0)
+    inline long syscall(long _id, long _arg0 = 0, long _arg1 = 0, long _arg2 = 0, long _arg3 = 0, long _arg4 = 0, long _arg5 = 0)
     {
-        register long x8 __asm__("x8") = _type;
+        register long x8 __asm__("x8") = _id;
         register long x0 __asm__("x0") = _arg0;
         register long x1 __asm__("x1") = _arg1;
         register long x2 __asm__("x2") = _arg2;
@@ -20,23 +20,22 @@ namespace cmn::system::syscall
         (
             "svc #0"
             : "+r"(x0)
-            : "r"(x8),
-            "r"(x1),
-            "r"(x2),
-            "r"(x3),
-            "r"(x4),
-            "r"(x5)
+            :  "r"(x8),
+               "r"(x1),
+               "r"(x2),
+               "r"(x3),
+               "r"(x4),
+               "r"(x5)
             : "memory"
         );
 
         return x0;
     }
-    #endif
-    #if CMN_SYSTEM_ARCH_X64
-    inline long syscall(long _type, long _arg0 = 0, long _arg1 = 0, long _arg2 = 0, long _arg3 = 0, long _arg4 = 0, long _arg5 = 0)
+    #elif CMN_SYSTEM_ARCH_X64
+    inline long syscall(long _id, long _arg0 = 0, long _arg1 = 0, long _arg2 = 0, long _arg3 = 0, long _arg4 = 0, long _arg5 = 0)
     {
         long _returnValue;
-        register long _rax __asm__("rax") = _type;
+        register long _rax __asm__("rax") = _id;
         register long _rdi __asm__("rdi") = _arg0;
         register long _rsi __asm__("rsi") = _arg1;
         register long _rdx __asm__("rdx") = _arg2;
@@ -47,13 +46,13 @@ namespace cmn::system::syscall
         (
             "syscall"
             : "=a"(_returnValue)
-            : "r"(_rax),
-                "r"(_rdi),
-                "r"(_rsi),
-                "r"(_rdx),
-                "r"(_r10),
-                "r"(_r8 ),
-                "r"(_r9 )
+            :  "r"(_rax),
+               "r"(_rdi),
+               "r"(_rsi),
+               "r"(_rdx),
+               "r"(_r10),
+               "r"(_r8 ),
+               "r"(_r9 )
             : "rcx", "r11", "memory"
         );
         return _returnValue;
--- script/xnu/xnu.sh
+++ script/xnu/xnu.sh
@@ -7,7 +7,7 @@ readonly PATH_OUT="$PATH_BIN/xnu.out"
 
 mkdir -p "$PATH_BIN"
 
-# git clone https://github.com/apple-oss-distributions/xnu.git "$PATH_XNU"
+git clone https://github.com/apple-oss-distributions/xnu.git "$PATH_XNU"
 
 clang++ "$PATH_PARSER" -O3 -std=c++23 -lclang -o "$PATH_OUT"
 $PATH_OUT
