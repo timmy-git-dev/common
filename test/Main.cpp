9badaf8d95da9595d8dc8d453271ebcce07cf223
timmy-git-dev <timmy.mail.dev@proton.me>
2026-07-12 13:14:24 -0600

    - windows hello world print test.

--- test/Main.cpp
+++ test/Main.cpp
@@ -13,8 +13,60 @@ i32 main(const i32, const c08**)
 }
 
 #elif CMN_SYSTEM_OS_WIN
+#include "system/nt/Func.hpp"
+
+struct PROCESS_BASIC_INFORMATION
+{
+    void* Reserved1;
+    void* PebBaseAddress;
+    void* Reserved2[2];
+    u64 UniqueProcessId;
+    void* Reserved3;
+};
+
+struct RTL_USER_PROCESS_PARAMETERS
+{
+    u08 Reserved1[0x20];
+    void* Reserved2[10];
+    HANDLE StandardInput;
+    HANDLE StandardOutput;
+    HANDLE StandardError;
+};
+
+struct PEB
+{
+    u08 Reserved1[0x20];
+    RTL_USER_PROCESS_PARAMETERS* ProcessParameters;
+};
+
 i32 main(const i32, const c08**)
 {
+    PROCESS_BASIC_INFORMATION pbi{};
+    NtQueryInformationProcess(
+        (HANDLE)-1,
+        (PROCESSINFOCLASS)0, // ProcessBasicInformation
+        &pbi,
+        sizeof(pbi),
+        __null
+    );
+
+    auto peb = (PEB*)pbi.PebBaseAddress;
+    HANDLE stdout = peb->ProcessParameters->StandardOutput;
+
+    static constexpr c08 text[] = "Hello, World!\n";
+
+    NtWriteFile(
+        stdout,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        (void*)text,
+        sizeof(text) - 1,
+        nullptr,
+        nullptr
+    );
+
     return 0;
 }
 #elif CMN_SYSTEM_OS_MAC
