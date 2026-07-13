7abbc0d96b71be62c91788859ead5b37889f0d66
timmy-git-dev <timmy.mail.dev@proton.me>
2026-07-13 17:03:12 -0600

    - main is called, duno if this will work?

--- test/Main.cpp
+++ test/Main.cpp
@@ -1,79 +1,93 @@
 #include "system/abi/Entry.hpp"
-#include "system/platform/OS.hpp"
+// #include "system/platform/OS.hpp"
 
-#if CMN_SYSTEM_OS_LIN
-#include "system/syscall/Call.hpp"
+// #if CMN_SYSTEM_OS_LIN
+// #include "system/syscall/Call.hpp"
 
-i32 main(const i32, const c08**)
-{
-    c08 _buffer[14] = "Hello, world!";
-    cmn::system::syscall::write(1, _buffer, 14);
+// i32 main(const i32, const c08**)
+// {
+//     c08 _buffer[14] = "Hello, world!";
+//     cmn::system::syscall::write(1, _buffer, 14);
 
-    return 0;
-}
+//     return 0;
+// }
 
-#elif CMN_SYSTEM_OS_WIN
+// #elif CMN_SYSTEM_OS_WIN
 #include "system/nt/Func.hpp"
 
-struct PROCESS_BASIC_INFORMATION
-{
-    void* Reserved1;
-    void* PebBaseAddress;
-    void* Reserved2[2];
-    u64 UniqueProcessId;
-    void* Reserved3;
-};
+typedef struct _IO_STATUS_BLOCK {
+    union {
+        NTSTATUS Status;
+        void *Pointer;
+    };
+    SIZE_T Information;
+} IO_STATUS_BLOCK;
 
-struct RTL_USER_PROCESS_PARAMETERS
-{
-    u08 Reserved1[0x20];
-    void* Reserved2[10];
+typedef struct _UNICODE_STRING {
+    USHORT Length;
+    USHORT MaximumLength;
+    wchar_t *Buffer;
+} UNICODE_STRING;
+
+typedef struct _RTL_USER_PROCESS_PARAMETERS {
+    unsigned char Reserved1[16];
+    void *Reserved2[10];
+    UNICODE_STRING ImagePathName;
+    UNICODE_STRING CommandLine;
     HANDLE StandardInput;
     HANDLE StandardOutput;
     HANDLE StandardError;
-};
+} RTL_USER_PROCESS_PARAMETERS;
 
-struct PEB
+typedef struct _PEB {
+    unsigned char Reserved1[24];
+    RTL_USER_PROCESS_PARAMETERS *ProcessParameters;
+} PEB;
+
+static PEB *get_peb(void)
 {
-    u08 Reserved1[0x20];
-    RTL_USER_PROCESS_PARAMETERS* ProcessParameters;
-};
-
-struct IO_STATUS_BLOCK
-{
-    union
-    {
-        i64 Status;
-        void* Pointer;
-    };
-
-    s64 Information;
-};
-
-struct _TEB
-{
-    void* Reserved1[12];
-    void* ProcessEnvironmentBlock;
-    void* Reserved2[399];
-};
+    PEB *peb;
+    __asm__ volatile (
+        "movq %%gs:0x60, %0"
+        : "=r"(peb)
+    );
+    return peb;
+}
 
 i32 main(const i32, const c08**)
 {
-    while(true)
-    {
-        continue;
-    }
+    static char msg[] = "Hello, world!\n";
+
+    PEB *peb = get_peb();
+
+    HANDLE stdout_handle =
+        peb->ProcessParameters->StandardOutput;
+
+    IO_STATUS_BLOCK iosb;
+
+    NtWriteFile(
+        stdout_handle,
+        0,
+        0,
+        0,
+        &iosb,
+        msg,
+        sizeof(msg) - 1,
+        0,
+        0
+    );
+
 
     return 0;
 }
-#elif CMN_SYSTEM_OS_MAC
-#include "system/xnu/Call.hpp"
+// #elif CMN_SYSTEM_OS_MAC
+// #include "system/xnu/Call.hpp"
 
-i32 main(const i32, const c08**)
-{
-    static constexpr c08 _text[] = "Hello, world!\n";
-    cmn::system::xnu::write(1, reinterpret_cast<user_addr_t>(_text), sizeof(_text) - 1);
+// i32 main(const i32, const c08**)
+// {
+//     static constexpr c08 _text[] = "Hello, world!\n";
+//     cmn::system::xnu::write(1, reinterpret_cast<user_addr_t>(_text), sizeof(_text) - 1);
 
-    return 0;
-}
-#endif
+//     return 0;
+// }
+// #endif
